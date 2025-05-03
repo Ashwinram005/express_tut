63214bc7822cd73d1b3d2c514884608a5eebc23e
Ashwinram005 <ashwinrammohan2005@gmail.com>
2025-05-03 19:07:35 +0530

    updated

--- express-app/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render("user", { title: "Users page", message: "Users listed" });
-});
-
-module.exports = router;
